3eaf821b896cf200245477c042a71a909104e007
Tom Witkowski <dev.gummibeer@gmail.com>
2017-11-02 17:36:23 +0100

    Update gif.php

--- gif.php
+++ gif.php
@@ -2,19 +2,75 @@
 require '../vendor/autoload.php';
 
 use Intervention\Image\ImageManager;
+use Symfony\Component\HttpFoundation\Request;
+use Intervention\Image\AbstractFont;
+use Carbon\Carbon;
 
-$manager = new ImageManager(array('driver' => 'imagick'));
+$request = Request::createFromGlobals();
 
+// basic setting
+$timezone = $request->get('tz', 'Europe/Berlin');
+$now = Carbon::parse('now', $timezone);
+$target = Carbon::parse($request->get('t', 'now'), $timezone);
+$diff = $now->diffInSeconds($target, false);
+$seconds = max(0, min(60, $request->get('s', 10)));
+$default = $request->get('d');
+$format = $request->get('f', 'd:h:m:s');
+
+// image settings
+$width = intval($request->get('w', 500));
+$height = intval($request->get('h', 50));
+$background = $request->get('bg', '#ffffff');
+
+// font settings
+$fontType = $request->get('ft', 'lato');
+$fontSize = intval($request->get('fs', 48));
+$fontColor = $request->get('fc', '#ff0000');
+switch ($fontType) {
+    case 'lato':
+        $fontFile = realpath('../resources/assets/fonts/lato-regular.ttf');
+        break;
+    default:
+        $fontFile = realpath('../resources/assets/fonts/lato-regular.ttf');
+        break;
+}
+
+/*
+ * do not touch this
+ * image generation by url parameters
+ */
+function secondsToUnits($seconds) {
+    $days = floor($seconds / (24 * 60 * 60));
+    $seconds -= $days * (24 * 60 * 60);
+    $hours = floor($seconds / (60 * 60));
+    $seconds -= $hours * (60 * 60);
+    $minutes = floor($seconds / 60);
+    $seconds -= $minutes * 60;
+
+    return [
+        'd' => str_pad($days, 2, '0', STR_PAD_LEFT),
+        'h' => str_pad($hours, 2, '0', STR_PAD_LEFT),
+        'm' => str_pad($minutes, 2, '0', STR_PAD_LEFT),
+        's' => str_pad($seconds, 2, '0', STR_PAD_LEFT),
+    ];
+}
+
+$seconds = min($seconds, max(0, $diff));
+
+$manager = new ImageManager(['driver' => 'imagick']);
 $gif = new Imagick();
-$gif->setFormat("gif");
-
-$seconds = 60;
-for ($i = 0; $i <= $seconds; ++$i) {
-    $frame = $manager->canvas(500, 50, '#ffffff');
-    $frame->text(str_pad($seconds-$i, 4, '0', STR_PAD_LEFT), 250, 25, function($font) {
-        $font->file(realpath('../resources/assets/fonts/lato-regular.ttf'));
-        $font->size(48);
-        $font->color('#ff0000');
+$gif->setFormat('gif');
+for ($i = 0; $i <= $seconds; $i++) {
+    $text = $default;
+    if($diff >= 0) {
+        $units = secondsToUnits($diff - $i);
+        $text = str_replace(array_keys($units), array_values($units), $format);
+    }
+    $frame = $manager->canvas($width, $height, $background);
+    $frame->text($text, $width / 2, $height / 2, function (AbstractFont $font) use ($fontFile, $fontSize, $fontColor) {
+        $font->file($fontFile);
+        $font->size($fontSize);
+        $font->color($fontColor);
         $font->align('center');
         $font->valign('center');
     });
